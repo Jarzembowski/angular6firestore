df0e09e24d0d299069c2ab1c46d07add8d704a6a
Jarzembowski <jargemboski@gmail.com>
2020-05-03 14:16:45 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: "AIzaSyBbqoCwStebFN987vcI-VI70IkSMtzSC4Y",
-    authDomain: "employee-85ec4.firebaseapp.com",
-    databaseURL: "https://employee-85ec4.firebaseio.com",
-    projectId: "employee-85ec4",
-    storageBucket: "employee-85ec4.appspot.com",
-    messagingSenderId: "539492848883"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   },
   firebaseDocs: {
-      user: 'users/khgobr9uzmHPlQ7Y2kWu'
+      user: ''
     }
 };
